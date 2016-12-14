b5e396269b886fdd7bf2233b6eadcf56b46d3469
Zeh Fernando <zeh@work.co>
2016-12-14 17:02:08 -0500

    Added more tests

--- test/.setup.js
+++ test/.setup.js
@@ -3,13 +3,13 @@ require("babel-core/register")();
 require("jsdom-global")();
 
 // requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
-// For tests only
-// By Paul Irish: https://gist.github.com/paulirish/1579671
+// https://gist.github.com/paulirish/1579671
+// For testing only; adapted for our current use to simulate 60 fps
 
 // MIT license
 
 (function() {
-	var lastTime = 0;
+	var desiredFPS = 60;
 	var vendors = ["ms", "moz", "webkit", "o"];
 	for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
 		window.requestAnimationFrame = window[vendors[x] + "RequestAnimationFrame"];
@@ -20,10 +20,10 @@ require("jsdom-global")();
 	if (!window.requestAnimationFrame) {
 		window.requestAnimationFrame = function(callback, element) {
 			var currTime = new Date().getTime();
-			var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-			var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-			  timeToCall);
-			lastTime = currTime + timeToCall;
+			var timeToCall = Math.max(0, 1000 / desiredFPS);
+			var id = window.setTimeout(function() {
+				callback(currTime + timeToCall);
+			}, timeToCall);
 			return id;
 		};
 	}
--- test/unit/FrameTicker-test-es6.js
+++ test/unit/FrameTicker-test-es6.js
@@ -10,4 +10,66 @@ describe("FrameTicker (ES6)", () => {
 		expect(new FrameTicker()).to.be.defined;
 		expect(new FrameTicker()).to.be.an.instanceof(FrameTicker);
 	});
+
+	it("starts paused", function(done) {
+		this.timeout(4 * 1000);
+
+		const t = new FrameTicker(NaN, NaN, true);
+		let c = 0;
+		t.onTick.add((s, d, f) => {
+			c++;
+		});
+		setTimeout(function() {
+			t.dispose();
+			expect(c).to.equal(0);
+			done();
+		}, 1000);
+	});
+
+	it("dispatches at about 60fps by default", function(done) {
+		this.timeout(4 * 1000);
+
+		const t = new FrameTicker();
+		let c = 0;
+		t.onTick.add((s, d, f) => {
+			c++;
+		});
+		setTimeout(function() {
+			t.dispose();
+			expect(c).to.be.within(57, 61);
+			done();
+		}, 1000);
+	});
+
+	it("dispatches at about 30fps", function(done) {
+		this.timeout(4 * 1000);
+
+		const t = new FrameTicker(30);
+		let c = 0;
+		t.onTick.add((s, d, f) => {
+			c++;
+		});
+		setTimeout(function() {
+			t.dispose();
+			expect(c).to.be.within(27, 31);
+			done();
+		}, 1000);
+	});
+
+	it("dispatches at about 120fps", function(done) {
+		this.timeout(4 * 1000);
+
+		const t = new FrameTicker(NaN, 120);
+		let c = 0;
+		t.onTick.add((s, d, f) => {
+			c++;
+		});
+		setTimeout(function() {
+			t.dispose();
+			expect(c).to.be.within(115, 121);
+			done();
+		}, 1000);
+	});
+
+	// TODO: timeScale; onPause; onResume; onTickOncePerFrame; animateOnce;
 });
